1559bac321b9344ca60df0462c32ce5521c676e7
Luke Randall <luke.randall@gmail.com>
2014-06-12 17:56:25 +0200

    Clean up Testbed

--- src/Data/Aeson/Forms/Testbed.hs
+++ src/Data/Aeson/Forms/Testbed.hs
@@ -3,21 +3,14 @@
 module Data.Aeson.Forms.Testbed
     ( User (..)
     , Person (..)
-    , success
     , A.decode
-    , key
-    , (^?)
     , values
-    , (.:)
-    , (.:?)
-    , errors
-    , text
-    , string
+    , module Data.Aeson.Forms.Combinators
+    , module Control.Applicative
     ) where
 
-import           Control.Lens.Fold ((^?))
+import           Control.Applicative
 import qualified Data.Aeson as A
-import           Data.Aeson.Lens (key)
 import           Data.Text (Text)
 ------------------------------------------------------------------------------
 import           Data.ByteString.Lazy.Char8 as BS
@@ -25,10 +18,10 @@ import           Data.Aeson.Forms.Combinators
 
 data User = User Text Text deriving (Show)
 
-data Person = Person Text Text (Maybe Text) deriving (Show)
+data Person = Person Text Text (Maybe Text) Bool deriving (Show)
 
 js :: BS.ByteString
-js = BS.pack "{\"name\":\"Luke\",\"last_name\":\"Randall\",\"age\":29}"
+js = BS.pack "{\"name\":\"Luke\",\"last_name\":\"Randall\",\"nickname\":\"ldrndll\",\"age\":29,\"is_client\":true}"
 
 values :: Maybe A.Value
 values = A.decode js
